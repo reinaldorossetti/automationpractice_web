f72241f7e739aea19e53f7ec90085ce842058bee
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2019-05-29 10:05:31 -0300

    deixando mais clear

--- features/support/env.rb
+++ features/support/env.rb
@@ -19,16 +19,6 @@ $total = nil # vai armazenar o total do produto no site.
 ENVIRONMENT_TYPE = ENV['ENVIRONMENT_TYPE']
 HEADLESS = ENV['HEADLESS']
 
-# Configuracao dos arquivos de log gerado.
-date_time = DateTime.now
-directory_name = "log"
-Dir.mkdir(directory_name) unless File.exists?(directory_name)
-$logger_name = "Log_file_#{date_time.to_s.gsub(":", "-")}.log"
-file = File.open("log/#{$logger_name}", 'w+')
-$logger = Logger.new(file)
-$logger.level = Logger::ERROR
-$logger.datetime_format = '%d-%m-%Y %H:%M:%S'
-
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/config/#{ENVIRONMENT_TYPE}.yml")
 
 ## register driver according with browser chosen
