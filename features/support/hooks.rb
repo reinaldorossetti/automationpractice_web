545565f837fe623d7a80735e1c6603da35845d1c
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2019-08-24 14:57:28 -0300

    Tirei o log para ficar mais simples para os iniciantes...
    Tirei o log para ficar mais simples para os iniciantes...

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,7 +2,6 @@ require_relative '../framework/helper.rb'
 
 Before do |scenario|
   include Capybara::DSL
-  $logs = page.driver.browser.manage.logs.get(:browser)
 end
 
 After do |scenario|
@@ -14,16 +13,5 @@ After do |scenario|
   else
     take_screenshot(file_name: scenario_name.downcase!, test_result: 'passed')
   end
-  $logger.close
-  path = "log/" + $logger_name
-  FileUtils.rm_f(path) if File.empty?(path)
 end
 
-
-def add_browser_logs
-  # Getting current URL
-  current_url = Capybara.current_url.to_s
-  # Remove warnings and info messages
-  $logs.reject! { |line| ['INFO'].include?(line.first) }
-  $logger.error("Current URL: " + current_url.to_s + "\n"  + $logs.to_s) unless $logs.empty?
-end
